cd821211e87a18ad462956334662c66baecbb0e8
Nick <ntiliakos07@gmail.com>
2024-10-02 10:14:41 +0200

    Delete unnecessary .mat files

--- code-files/DMD.m
+++ code-files/DMD.m
@@ -1,7 +1,9 @@
 %% DMD
+
 clear all
 close all
 tic
+
 %%
 % Load the raw data from the installation of the 3rd pile. For the 5 first
 % geophones. Only vertical velocity is considered.
@@ -9,6 +11,7 @@ tic
 % Seq1 -> 1.55 - 2.00 min; penetration 0.9 - 1.0 m (approx)
 % Seq2 -> 4.40 - 4.45 min; penetration 1.75 - 2.0 m (approx)
 % Seq3 -> 10.55 - 11.0 min; penetration 3.50 - 3.60 (approx)
+
 name = "Seq1";
 load(name+".mat")
 
