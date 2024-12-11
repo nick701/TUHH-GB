b90c4392132c7794b285efa721bba93f8aa8d1af
Nick <ntiliakos07@gmail.com>
2024-12-11 11:04:55 +0100

    FB_DMD code working correctly. Got rid of Test_FB_DMD.

--- Denoising/FB_DMD.m
+++ Denoising/FB_DMD.m
@@ -15,17 +15,16 @@ G4 = downsample(G4V3, n);
 G5 = downsample(G5V3, n);
 time = downsample(Time, n);
 
-% Calculate time delta
 dt = time(2) - time(1);
 
 f_all = [G1'; G2'; G3'; G4'; G5']';
 
 % Parameters
-r = 5; % Truncation rank
+r = 4; % Truncation rank
 plot_live = 1; % Enable live plotting
 plot_error = 1; % Enable error plotting
 
-window = 500; % Window length
+window = 500; 
 step = 100; % Step size for overlapping windows
 
 for start_idx = 1:step:(size(f_all, 1) - window)
@@ -33,7 +32,8 @@ for start_idx = 1:step:(size(f_all, 1) - window)
     Y = f_all(start_idx:start_idx+window-1, :)';
     Yp = Y(:, 2:end);
     Ym = Y(:, 1:end-1);
-    
+    tic
+    toc
     % Forward DMD
     [U_f, S_f, V_f] = svd(Ym, 'econ');
     f_Atilde = U_f' * Yp * V_f / S_f;
@@ -47,18 +47,15 @@ for start_idx = 1:step:(size(f_all, 1) - window)
     [W, D] = eig(Atilde);
     Phi = Yp * V_f / S_f * W;
     
-    % DMD Spectra
     lambda = diag(D);
     omega = log(lambda) / dt;
     
-    % Initial condition
     b = Phi \ Ym(:, 1);
     
     % Reconstructed signal
     time_recon = 0:dt:(window-1)*dt;
     Y_recon = real(Phi * (b .* exp(omega * time_recon)));
     
-    % Live plotting
     if plot_live
         for geo = 1:size(Y, 1)
             subplot(3, 2, geo);
@@ -74,7 +71,6 @@ for start_idx = 1:step:(size(f_all, 1) - window)
         drawnow;
     end
     
-    % Compute and plot error
     if plot_error
         NRMSE = sqrt(mean((Y(:) - Y_recon(:)).^2)) / (max(Y(:)) - min(Y(:)));
         subplot(3, 2, 6);
